43eb141383993d1439ec32a6c06a4bf245ed1b84
Joel Larson <519553+JoelLarson@users.noreply.github.com>
2023-01-03 03:43:39 -0700

    React: Update dependencies to the latest versions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 
-//import React from "react";
 import React from 'react';
 import Html5QrcodePlugin from "./Html5QrcodePlugin.jsx";
 import ResultContainerPlugin from "./ResultContainerPlugin.jsx";
